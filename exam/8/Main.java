fd319b4cb02306cca93c485db7d2ca0dc6ee7e34
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2024-11-29 03:54:26 +0530

    java servlets

---
+++ exam/8/Main.java
@@ -0,0 +1,11 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Main extends HttpServlet {
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String name = req.getParameter("name");
+    resp.setContentType("text/html");
+    resp.getWriter().println("<h1>Hello, " + name + "!</h1>");
+  }
+}
